c21a311de439a7184c230258333b38aaef688519
luke-h1 <luke.howsam@yahoo.com>
2020-07-29 17:04:43 +0100

    commiting before everything goes tits up (probably)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const API_URL = 'https://anime-chan.herokuapp.com/api/quotes/random';
+
+// generic variables
 const button = document.querySelector('.btn');
 const card = document.querySelector('.card');
+
+// results from API variables
+
+const show = document.querySelector('.card-title'); //output where the series / show will go
+const quote = document.querySelector('.card-text'); // output where the quote will go
+const author = document.querySelector('.author'); // output where character will go (span)
 
 button.addEventListener('click', getData);
 
