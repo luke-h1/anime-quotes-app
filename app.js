b597e41a24fe8b2fcc3da0b17bb360b59eca6f09
luke-h1 <luke.howsam@yahoo.com>
2020-08-29 16:32:42 +0100

    update

--- app.js
+++ app.js
@@ -2,38 +2,43 @@ const searchBtn = document.getElementById('search-btn');
 const searchTerm = document.getElementById('term');
 const errorEl = document.getElementById('error');
 const form = document.getElementById('form-control');
+const quotes = document.getElementById('quotes');
+
 function showError(message) {
   errorEl.innerHTML = message;
 }
 
+
 function getAnimeData(e) {
-  e.preventDefault(); 
-  if (searchTerm === null) {
-    showError('<h2>Please enter a valid anime</h2>');
-  } else { 
-    let query = searchTerm.value; 
-    const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`;
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        
-      });
-  }
+  e.preventDefault();
+  const query = searchTerm.value;
+  const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`;
+  fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      quotes.innerHTML = data.quote.map(
+        (anime) => `
+          <div class="row"> 
+            <div class="col s12 m6"> 
+              <div class="Card blue-grey darken-1"> 
+                <div class="card-content white-text"> 
+                  <span class="card-title">${anime.character}</span> 
+                    <p>${anime.quote}</p> 
+                  </div> 
+                    <div class="card-action"> 
+                      <a href="#">${anime.anime}</a> 
+                      </div> 
+                        </div> 
+                          </div> 
+                            </div>
+                            `,
+
+      )
+        .join('');
+    });
 }
 
-// async function getAnimeData(e) {
-//   e.preventDefault();
-//   if (searchTerm.value === null) {
-//     showError('<h2>Please enter a valid anime</h2>');
-//   } else {
-//     const API_URL = `https://anime-chan.herokuapp.com/api/quotes?=${searchTerm}`;
-//     await fetch(API_URL)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data);
-//       });
-//   }
-// }
 
 // EVENT LISTENERS
 form.addEventListener('submit', getAnimeData);
