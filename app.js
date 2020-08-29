48a47a1e8d94d5fdd665ebe029686e57ea828be1
luke-h1 <luke.howsam@yahoo.com>
2020-08-29 17:06:22 +0100

    add loading element

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const searchTerm = document.getElementById('term');
 const errorEl = document.getElementById('error');
 const form = document.getElementById('form-control');
 const quotes = document.getElementById('quotes');
+const imageLoadingEl = document.getElementById('loading'); 
 
 function showError(message) {
   errorEl.innerHTML = message;
@@ -12,30 +13,34 @@ function showError(message) {
 function getAnimeData(e) {
   e.preventDefault(e);
   const query = searchTerm.value;
-  const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`;
+  const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`; 
+  imageLoadingEl.style.display = 'block';  
   fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-      quotes.innerHTML = data.data.map(
-        (anime) => `
+      quotes.innerHTML = data.data
+        .map(
+          (anime) => `
         <div class="row">
         <div class="col s12 m6">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
-              <span class="card-title">${anime.character}</span>
+              <span class="card-title">${anime.anime}</span>
               <p>${anime.quote}</p>
-                <div class="card-action">
-                  <a href="#">${anime.anime}</a>
+                <div class="card-action"> 
+                ${anime.character}</a>
                   </div>
                   </div>
                   </div>
                   </div>
-  
-        `, 
-      ) 
-      .join(''); 
-    });
-}
+        `
+        )
+        .join('');
+        imageLoadingEl.style.display = 'none'; 
+
+    }); 
+
+  }
 
 // function getAnimeData(e) {
 //   e.preventDefault(e);
