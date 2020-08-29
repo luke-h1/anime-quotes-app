605ace71e8b4e7e77e4f6f517e7d8edd2f65f3a9
luke-h1 <luke.howsam@yahoo.com>
2020-08-29 16:55:26 +0100

    map function working now

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const searchBtn = document.getElementById('search-btn');
 const searchTerm = document.getElementById('term');
 const errorEl = document.getElementById('error');
@@ -8,37 +9,61 @@ function showError(message) {
   errorEl.innerHTML = message;
 }
 
-
 function getAnimeData(e) {
-  e.preventDefault();
+  e.preventDefault(e);
   const query = searchTerm.value;
   const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`;
   fetch(API_URL)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      quotes.innerHTML = data.quote.map(
+      quotes.innerHTML = data.data.map(
         (anime) => `
-          <div class="row"> 
-            <div class="col s12 m6"> 
-              <div class="Card blue-grey darken-1"> 
-                <div class="card-content white-text"> 
-                  <span class="card-title">${anime.character}</span> 
-                    <p>${anime.quote}</p> 
-                  </div> 
-                    <div class="card-action"> 
-                      <a href="#">${anime.anime}</a> 
-                      </div> 
-                        </div> 
-                          </div> 
-                            </div>
-                            `,
-
-      )
-        .join('');
+        <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">${anime.character}</span>
+              <p>${anime.quote}</p>
+                <div class="card-action">
+                  <a href="#">${anime.anime}</a>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+  
+        `, 
+      ) 
+      .join(''); 
     });
 }
 
+// function getAnimeData(e) {
+//   e.preventDefault(e);
+//   const query = searchTerm.value;
+//   const API_URl = `https://animechanapi.xyz/api/quotes?anime=${query}`;
+//   fetch(API_URl)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const arr = data;
+//       arr.forEach((item, index) => {
+//         quotes.innerHTML = `
+//         <div class="row">
+//           <div class="col s12 m6">
+//             <div class="card blue-grey darken-1">
+//               <div class="card-content white-text">
+//                 <span class="card-title">${arr.character[`${index}`]}</span>
+//                 <p>${arr.quote[`${index}`]}</p>
+//                   <div class="card-action">
+//                     <a href="#">${arr.anime[`${index}`]}</a>
+//                     </div>
+//                     </div>
+//                     </div>
+//                     </div>
+// //                     `;
+// //       });
+// //     });
+// // }
 
-// EVENT LISTENERS
+// // EVENT LISTENERS
+
 form.addEventListener('submit', getAnimeData);
