78368af5576f99f89e678827fdd2ad513acffab5
luke-h1 <luke.howsam@yahoo.com>
2020-08-29 19:14:43 +0100

    improve styles

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ function getAnimeData(e) {
     .then((data) => {
       quotes.innerHTML = data.data
         .map(
-          (anime) => `
+          (anime) => ` 
         <div class="row">
         <div class="col s2 m12 center">
           <div class="card blue-grey darken-1">
             <div class="card-content white-text">
-              <span class="card-title">${anime.anime}</span>
+              <span class="card-title">Character:<br>${anime.anime}</span>
               <p>${anime.quote}</p>
-                <div class="card-action"> 
-                ${anime.character}</a>
+                <div class="card-action white-text"> 
+                Character:<br>${anime.character}
                   </div>
                   </div>
                   </div>
@@ -55,10 +55,10 @@ function getRandomQuote(e) {
         <div class="col s2 m12 center">
           <div class="card blue-grey ">
             <div class="card-content white-text">
-              <span class="card-title quote-title">${randomAnime.anime}</span>
+              <span class="card-title quote-title">Show: <br>${randomAnime.anime}</span>
               <p class="quote">${randomAnime.quote}</p>
                 <div class="card-action">  
-                <p class="character">${randomAnime.character}</p> 
+                <p class="character">Character: <br>${randomAnime.character}</p> 
                   </div>
                   </div>
                   </div>
