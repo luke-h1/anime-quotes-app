09ceb6c7c1b285425c1d6d537a21f0c1a872d8d1
luke-h1 <luke.howsam@yahoo.com>
2020-08-31 12:20:57 +0100

    add filter functionality :

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-plusplus */
 const quotesContainer = document.getElementById('quotes-container');
 const loading = document.querySelector('.loader');
 const imageLoadingEl = document.getElementById('loading');
-// const filter = document.getElementById('filter');
+const filter = document.getElementById('filter');
 
 let page = 2;
 
@@ -23,14 +24,15 @@ async function showQuotes() {
   const quotes = await getQuotes();
   console.log(quotes);
   const quoteEl = document.createElement('div');
-  quoteEl.innerHTML = quotes.data.map(
-    (anime) => `
+  quoteEl.innerHTML = quotes.data
+    .map(
+      (anime) => `
   <div class="row">
   <div class="col s2 m12 center">
     <div class="card blue-grey darken-1">
       <div class="card-content white-text">
         <span class="card-title">Anime:<br>${anime.anime}</span>
-        <p>${anime.quote}</p>
+        <p class="anime-quote">${anime.quote}</p>
           <div class="card-action white-text"> 
           Character:<br>${anime.character}
           </div>
@@ -39,9 +41,9 @@ async function showQuotes() {
                       </div>
   
   `,
-  )
+    )
     .join('');
-    quotesContainer.appendChild(quoteEl);
+  quotesContainer.appendChild(quoteEl);
 }
 // quotesContainer.appendChild(quoteEl);
 showQuotes();
@@ -58,6 +60,25 @@ function showLoader() {
   }, 1000);
 }
 
+function filterQuotes(e) {
+  const term = filter.value.toUpperCase();
+  const items = document.querySelectorAll('.row');
+  console.log(items);
+  items.forEach((item) => {
+    const anime = item.querySelector('.card-title').innerText.toUpperCase();
+    const quote = item.querySelector('.anime-quote').innerText.toUpperCase();
+    const character = item.querySelector('.card-action').innerText.toUpperCase();
+    if (anime.indexOf(term) > -1 || quote.indexOf(term) > -1 || character.indexOf(term) > -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+filter.addEventListener('input', filterQuotes);
+
+// EVENT LISTENERS
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
   if (scrollTop + clientHeight >= scrollHeight - 5) {
