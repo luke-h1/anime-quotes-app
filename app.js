62c4853710af7489d2defe499a2c4a2c795ab831
luke-h1 <luke.howsam@yahoo.com>
2020-08-31 12:07:09 +0100

    inifinite scroll now working..

--- app.js
+++ app.js
@@ -25,18 +25,18 @@ function getAnimeData(e) {
       quotes.innerHTML = data.data
         .map(
           (anime) => ` 
-        <div class="row">
-        <div class="col s2 m12 center">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Character:<br>${anime.anime}</span>
-              <p>${anime.quote}</p>
-                <div class="card-action white-text"> 
-                Character:<br>${anime.character}
+          <div class="row">
+          <div class="col s2 m12 center">
+            <div class="card blue-grey darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Anime:<br>${anime.anime}</span>
+                <p>${anime.quote}</p>
+                  <div class="card-action white-text"> 
+                  Character:<br>${anime.character}
                   </div>
-                  </div>
-                  </div>
-                  </div>
+                      </div>
+                          </div>
+                              </div>
         `,
         )
         .join('');
--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,50 +1,66 @@
-const quotesContainer = document.getElementByIdl('quotes-container');
+/* eslint-disable no-unused-vars */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-plusplus */
+const quotesContainer = document.getElementById('quotes-container');
 const loading = document.querySelector('.loader');
-const filter = document.getElementById('filter');
+const imageLoadingEl = document.getElementById('loading');
+// const filter = document.getElementById('filter');
 
-const page = 1;
+let page = 2;
 
 async function getQuotes() {
-  const API_URL = `https://aniomechanapi.xyz/api/quotes/page=${page}`;
+  const API_URL = `https://animechanapi.xyz/api/quotes/?page=${page}`;
   const res = await fetch(API_URL);
   const json = await res.json();
   return json;
 }
 
+// const quoteEl = document.createElement('div');
+// quoteEl.classList.add('quote');
+// quoteEl.innerHTML = quote.data.map
 
-async function showQuotes(){
-  const quotes = await getQuotes(); 
-  quotes.forEach((post) => {
-    const quoteEl = document.createElement('div'); 
-    quoteEl.classList.add('quote');
-    quoteEl.innerHTML = `
-      <div class="number">${}</div> 
-        <div class="quote-info">
-          <h2 class="quote-title">${}</h2>
-        </div> 
-        <p class="quote-body">${}</p> 
-    `; 
-    quotesContainer.appenChild(quoteEl);
-  }); 
-  showQuotes();
+async function showQuotes() {
+  const quotes = await getQuotes();
+  console.log(quotes);
+  const quoteEl = document.createElement('div');
+  quoteEl.innerHTML = quotes.data.map(
+    (anime) => `
+  <div class="row">
+  <div class="col s2 m12 center">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">Anime:<br>${anime.anime}</span>
+        <p>${anime.quote}</p>
+          <div class="card-action white-text"> 
+          Character:<br>${anime.character}
+          </div>
+              </div>
+                  </div>
+                      </div>
+  
+  `,
+  )
+    .join('');
+    quotesContainer.appendChild(quoteEl);
 }
-
-
-function showLoading(){
+// quotesContainer.appendChild(quoteEl);
+showQuotes();
+// show loader + fetch more quotes
+function showLoader() {
   loading.classList.add('show');
-  setTimeout(() => { 
-    page++; 
-    showQuotes();
-  }, 300)
   setTimeout(() => {
-    loading.classList.remove('show'); 
-  }, 1000)
+    page++;
+    showQuotes();
+  }, 300);
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+  }, 1000);
 }
 
-function filterQuotes(e){
-  const term = e.target.value.toUpperCase(); 
-  const quotes2 = document.querySelectorAll('quoteEl')
-  quotes2.forEach((quote) => { 
-    
-  })
-}
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoader();
+  }
+});
