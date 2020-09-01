3774a53e59f3f50f397d64ebb9b4afe61fff8cd7
luke-h1 <luke.howsam@yahoo.com>
2020-09-01 18:48:39 +0100

    refactor random functionality to use async/await

--- app.js
+++ app.js
@@ -5,33 +5,28 @@ const quotes = document.getElementById('quotes');
 const imageLoadingEl = document.getElementById('loading');
 const randomQuoteBtn = document.getElementById('random-btn');
 const searchbtn = document.getElementById('search-btn');
-function showError(message){
+function showError(message) {
   errorEl.innerHTML = message;
-  setTimeout(() => { 
-    errorEl.innerHTML = ''
-  }, 2000)
+  setTimeout(() => {
+    errorEl.innerHTML = '';
+  }, 2000);
 }
 
+async function getAnimeData() {
+  const query = searchTerm.value;
+  const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}`;
+  if (query === '') {
+    showError('Enter a correct value 🤷‍♂️');
+  } else {
+    imageLoadingEl.style.display = 'block';
+    const res = await fetch(`${API_URL}`);
+    const data = await res.json();
+    console.log(data);
+    showAnime(data);
+  }
+}
 
-
-
-
-async function getAnimeData(){ 
-  const query = searchTerm.value; 
-  const API_URL = `https://animechanapi.xyz/api/quotes?anime=${query}` 
-  if (query === ''){
-    showError('Enter a correct value 🤷‍♂️')
-  }else {  
-  imageLoadingEl.style.display = 'block';
-  const res = await fetch(`${API_URL}`);
-  const data = await res.json(); 
-  console.log(data); 
-  showAnime(data);
-  } 
-}; 
-
-
-function showAnime(data) { 
+function showAnime(data) {
   let output = '';
   data.data.forEach((quote) => {
     output += `
@@ -47,40 +42,39 @@ function showAnime(data) {
           </div>
           </div> 
     `;
-  })
+  });
   imageLoadingEl.style.display = 'none';
   quotes.innerHTML = output;
 }
 
-
-
-function getRandomQuote(e) {
-  e.preventDefault();
-  const RANDOM_API_URL = 'https://animechanapi.xyz/api/quotes/';
+async function getRandomQuote() {
+  const RAN_API_URL = 'https://animechanapi.xyz/api/quotes/';
   imageLoadingEl.style.display = 'block';
-  fetch(RANDOM_API_URL)
-    .then((res) => res.json())
-    .then((data) => {
-      quotes.innerHTML = '';
-      quotes.innerHTML = data.data
-        .map(
-          (randomAnime) => `  
-          <div class="quotes"> 
+  const res = await fetch(`${RAN_API_URL}`);
+  const randomData = await res.json();
+  console.log(randomData);
+  showRandomAnime(randomData);
+}
+
+function showRandomAnime(randomData) {
+  let randomOutput = '';
+  randomData.data.forEach((quote) => {
+    randomOutput += `
+    <div class="quotes"> 
           <div class="container">
-            <h6 class="post-title"><span class="style-card">Anime:</span> <br>${randomAnime.anime}</h6>
+            <h6 class="post-title"><span class="style-card">Anime:</span> <br>${quote.anime}</h6>
             <p class="post-intro">
-              <span class="style-card">Quote:</span> <br>${randomAnime.quote}
+              <span class="style-card">Quote:</span> <br>${quote.quote}
             </p> 
             <br>
             <hr>
-            <p><span class="char-style">Character:</span> <br>${randomAnime.character}</p>
+            <p><span class="char-style">Character:</span> <br>${quote.character}</p>
               </div>
               </div> 
-        `
-        )
-        .join('');
-      imageLoadingEl.style.display = 'none';
-    });
+    `;
+  });
+  quotes.innerHTML = randomOutput;
+  imageLoadingEl.style.display = 'none';
 }
 
 // // EVENT LISTENERS
